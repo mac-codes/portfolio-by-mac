c4448e214fbacbd5e1df6e0cb41b969b44c51a9c
McKenna Lawson <mckenna.lawson01@gmail.com>
2023-03-09 16:22:11 -0600

    gh-pages fixes

--- src/App.js
+++ src/App.js
@@ -7,23 +7,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 function App() {
- 
-   return(
-     <>
-     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path='about' element={<About/>} />
-          <Route path='projects' element={<Projects />} />
-          <Route path='contact' element={<Contact />} />
-        </Route>
-      </Routes>
-     </BrowserRouter>
-     </>
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/bad-portfolio' element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='projects' element={<Projects />} />
+            <Route path='contact' element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
- 
-   
+
+
 }
 
 export default App;
