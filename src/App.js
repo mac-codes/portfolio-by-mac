1c5a98446cc012226e38d15cce3c0cff23b9f148
McKenna Lawson <mckenna.lawson01@gmail.com>
2023-03-09 16:36:59 -0600

    fixing changes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path='/bad-portfolio' element={<SharedLayout />}>
+          <Route path='/' element={<SharedLayout />}>
             <Route index element={<Home />} />
             <Route path='about' element={<About />} />
             <Route path='projects' element={<Projects />} />
             <Route path='contact' element={<Contact />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 
